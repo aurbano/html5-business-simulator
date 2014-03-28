34e86f96f07c097daa1a42f5ca76a18e07afd165
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-03-28 19:32:23 +0100

    d3js testing

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -17,7 +17,34 @@ var svg = d3.select("#animation").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("test.json", function(error, graph) {
+var palancas = [
+	{ "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+	{ "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+	{ "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+
+var circles = svg.selectAll("circle")
+                          .data(palancas)
+                          .enter()
+                          .append("circle");
+
+var circleAttributes = circles
+                       .attr("cx", function (d) { return d.x_axis; })
+                       .attr("cy", function (d) { return d.y_axis; })
+                       .attr("r", function (d) { return d.radius; })
+                       .style("fill", function(d) { return d.color; });
+
+var circlesTransition = d3.selectAll("circle")
+   .transition()
+   .duration(5000)
+   .attr("cy", "0")
+   .attrTween('cx', function (d, i, a) {
+        return function (t) {
+          // Add salt, pepper and constants as per your taste
+          return a + (Math.random() - 0.5) * 10;
+        };
+});
+
+/*d3.json("test.json", function(error, graph) {
   force
       .nodes(graph.nodes)
       .links(graph.links)
@@ -49,4 +76,4 @@ d3.json("test.json", function(error, graph) {
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   });
-});
+});*/
