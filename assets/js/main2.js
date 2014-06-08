8e4138b0a33f7569fcc2b65ef6d26f841a80b3ce
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-06-08 18:59:27 +0200

    Minor

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -10,12 +10,32 @@ Simulator = {
 	// Para arrancar el simulador hay que llamar a 'Simulator.start();''
 	config: {
 		debug: true, // Turn console debugging on/off
-		speed: 0.9, // Modificador de la velocidad. 1 : Normal; >1 : Más lento; <1 : Más rapido
-		wheel: 4, // Tiempo (s) que tarda en dar una vuelta entera la rueda, relativo a la velocidad del simulador
 	},
 
 	start: function(){
 		console.log('Empezando Simulador 2');
+
+		$(window).resize(function () {
+			Simulator.resize();
+		});
+
+		Simulator.resize();
+	},
+
+	resize: function () {
+		var simOff = $('#simulador').offset(),
+			footerOff = $('footer').offset(),
+			footerHeight = $('footer').height(),
+			win = $(window).height(),
+			height = Math.max(win - simOff.top - footerHeight - 50, 374);
+		$('#simulador').height(height);
+
+		$('.animation').each(function () {
+			var top = height / 2 - $(this).height() / 2 + 40;
+			$(this).css({
+				top: top
+			});
+		});
 	}
 
 };
